227aaf6f5ee4fe7219a3816110a306617525eee7
暮闲 <moxinqian_vip@163.com>
2023-03-20 22:17:57 +0800

    init project

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import getters from './getters'
+import app from './modules/app'
+import settings from './modules/settings'
+import user from './modules/user'
+import example from  './modules/example'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  modules: {
+    app,
+    settings,
+    user,
+    example
+  },
+  getters
+})
+
+export default store
