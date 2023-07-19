08cefabe9949b71450af419b7ae9437ddec708d4
暮闲 <moxinqian_vip@163.com>
2023-07-20 07:41:56 +0800

    Dashboard功能

---
+++ src/api/metrics.js
@@ -0,0 +1,10 @@
+import request from '@/utils/request'
+
+export function getDashboardData() {
+  let url = "/metrics/dashboard"
+  return request({
+    url: url,
+    method: "get",
+  })
+}
+
--- src/main.js
+++ src/main.js
@@ -15,6 +15,11 @@ import router from './router'
 import '@/icons' // icon
 import '@/permission' // permission control
 
+
+// 全局引入相关包
+import * as echarts from "echarts";
+// 开启echarts
+Vue.prototype.$echarts = echarts;
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
